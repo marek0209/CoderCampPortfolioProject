02fcdb50033b13bce9530904d3da02099b243165
Marek Urbaniec <marek.urbaniec1@gmail.com>
2019-11-01 20:48:23 +0100

    Add first section and particles.js

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,6 @@
 //Marek Urbaniec Portfolio custom JavaScript
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'js/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
